7cdabb5bee4551503ed14894302736d591005415
Felix-Johannes Jendrusch <felix@felixjendrusch.is>
2015-02-08 17:24:02 +0100

    Add utils specs

---
+++ PistachioTests/UtilsSpec.swift
@@ -0,0 +1,34 @@
+//
+//  UtilsSpec.swift
+//  Pistachio
+//
+//  Created by Felix Jendrusch on 2/8/15.
+//  Copyright (c) 2015 Felix Jendrusch. All rights reserved.
+//
+
+import Quick
+import Nimble
+
+import Pistachio
+
+class UtilsSpec: QuickSpec {
+    override func spec() {
+        describe("zip") {
+            it("should zip two arrays") {
+                // Tuples do not conform to Equatable.
+                let result = zip([ 1, 2 ], [ 2, 4, 6 ]).map { (a, b) in [ a, b ] }
+
+                expect(result).to(equal([ [ 1, 2 ], [ 2, 4 ] ]))
+            }
+        }
+
+        describe("unzip") {
+            it("should unzip an array of tuples") {
+                let (a, b) = unzip([ (1, 2), (2, 4) ])
+
+                expect(a).to(equal([ 1, 2 ]))
+                expect(b).to(equal([ 2, 4 ]))
+            }
+        }
+    }
+}
