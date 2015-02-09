afad8de992ef800b3cf259867231f57edfe9f4ab
Felix-Johannes Jendrusch <felix@felixjendrusch.is>
2015-02-09 07:55:36 +0100

    Add fixed adapters specs

--- PistachioTests/AdapterSpec.swift
+++ PistachioTests/AdapterSpec.swift
@@ -24,6 +24,17 @@ extension ValueTransformers {
         }
     })
 
+    static let array: ValueTransformer<[AnyObject], AnyObject, NSError> = ValueTransformer(transformClosure: { value in
+        return success(value)
+    }, reverseTransformClosure: { value in
+        switch value {
+        case let value as [AnyObject]:
+            return success(value)
+        default:
+            return failure(NSError())
+        }
+    })
+
     static let dictionary: ValueTransformer<[String: AnyObject], AnyObject, NSError> = ValueTransformer(transformClosure: { value in
         return success(value)
     }, reverseTransformClosure: { value in
@@ -36,6 +47,20 @@ extension ValueTransformers {
     })
 }
 
+struct Node {
+    var children: [Node]
+
+    init(children: [Node]) {
+        self.children = children
+    }
+}
+
+struct NodeLenses {
+    static let children = Lens<Node, [Node]>(get: { $0.children }, set: { (inout node: Node, children) in
+        node.children = children
+    })
+}
+
 struct Adapters {
     static let inner = DictionaryAdapter(specification: [
         "count": transform(InnerLenses.count, ValueTransformers.int)
@@ -45,6 +70,12 @@ struct Adapters {
         "count": transform(OuterLenses.count, ValueTransformers.int),
         "inner": transform(OuterLenses.inner, lift(inner, Inner(count: 0)))
     ], dictionaryTansformer: ValueTransformers.dictionary)
+
+    static let node: DictionaryAdapter<Node, AnyObject, NSError> = fix { adapter in
+        return DictionaryAdapter(specification: [
+            "children": transform(NodeLenses.children, lift(lift(adapter, Node(children: []))) >>> ValueTransformers.array)
+        ], dictionaryTansformer: ValueTransformers.dictionary)
+    }
 }
 
 class AdapterSpec: QuickSpec {
@@ -62,6 +93,22 @@ class AdapterSpec: QuickSpec {
                 let result = adapter.decode(Inner(count: 2), from: [ "count": 3 ])
 
                 expect(result.value?.count).to(equal(3))
+            }
+        }
+
+        describe("Fixed adapters") {
+            let adapter = Adapters.node
+
+            it("should encode a model") {
+                let result = adapter.encode(Node(children: [ Node(children: []), Node(children: []) ]))
+
+                expect((result.value as? [String: [AnyObject]])?["children"]?.count).to(equal(2))
+            }
+
+            it("should decode a model from data") {
+                let result = adapter.decode(Node(children: []), from: [ "children": [ [ "children": [] ], [ "children": [] ] ] ])
+
+                expect(result.value?.children.count).to(equal(2))
             }
         }
 
