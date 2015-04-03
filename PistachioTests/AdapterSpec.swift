440a2c0d8340a387d872b973795214e1eb1d3e80
Felix Jendrusch <felix@felixjendrusch.is>
2015-04-03 13:58:22 +0200

    Integrate felixjendrusch/ValueTransformer
    Please note that `Adapter` has been renamed to `AdapterType`.
    
    Closes #2.

--- PistachioTests/AdapterSpec.swift
+++ PistachioTests/AdapterSpec.swift
@@ -4,37 +4,48 @@ import Quick
 import Nimble
 
 import LlamaKit
+import ValueTransformer
 import Pistachio
 
-extension ValueTransformers {
-    static let int: ValueTransformer<Int, AnyObject, NSError> = ValueTransformer(transformClosure: { value in
-        return success(value)
-    }, reverseTransformClosure: { value in
-        switch value {
-        case let value as Int:
+struct ValueTransformers {
+    static let string = ReversibleValueTransformer<Int, String, NSError>(transformClosure: { value in
+        return success(String(value))
+    }, reverseTransformClosure: { transformedValue in
+        if let value = transformedValue.toInt() {
             return success(value)
+        } else {
+            return failure(NSError())
+        }
+    })
+
+    static let int = ReversibleValueTransformer<Int, AnyObject, NSError>(transformClosure: { value in
+        return success(value)
+    }, reverseTransformClosure: { transformedValue in
+        switch transformedValue {
+        case let transformedValue as Int:
+            return success(transformedValue)
         default:
             return failure(NSError())
         }
     })
 
-    static let array: ValueTransformer<[AnyObject], AnyObject, NSError> = ValueTransformer(transformClosure: { value in
+    static let array = ReversibleValueTransformer<[AnyObject], AnyObject, NSError>(transformClosure: { value in
         return success(value)
-    }, reverseTransformClosure: { value in
-        switch value {
-        case let value as [AnyObject]:
-            return success(value)
+    }, reverseTransformClosure: { transformedValue in
+        switch transformedValue {
+        case let transformedValue as [AnyObject]:
+            return success(transformedValue)
         default:
             return failure(NSError())
         }
     })
 
-    static let dictionary: ValueTransformer<[String: AnyObject], AnyObject, NSError> = ValueTransformer(transformClosure: { value in
+    static let dictionary = ReversibleValueTransformer<[String: AnyObject], AnyObject, NSError>(transformClosure: { value in
         return success(value)
-    }, reverseTransformClosure: { value in
-        switch value {
-        case let value as [String: AnyObject]:
-            return success(value)
+    }, reverseTransformClosure: { transformedValue in
+        switch transformedValue {
+        case let transformedValue as [String: AnyObject]:
+            return success(transformedValue)
         default:
             return failure(NSError())
         }
@@ -110,19 +121,19 @@ class AdapterSpec: QuickSpec {
             let adapter = lift(Adapters.inner, Inner(count: 3))
 
             it("should transform a value") {
-                let result = adapter.transformedValue(Inner(count: 4))
+                let result = adapter.transform(Inner(count: 4))
 
                 expect(result.value as? [String: Int]).to(equal([ "count": 4 ]))
             }
 
             it("should reverse transform a value") {
-                let result = adapter.reverseTransformedValue([ "count": 5 ])
+                let result = adapter.reverseTransform([ "count": 5 ])
 
                 expect(result.value?.count).to(equal(5))
             }
 
             it("should use the default structure") {
-                let result = adapter.reverseTransformedValue([String: AnyObject]())
+                let result = adapter.reverseTransform([String: AnyObject]())
 
                 expect(result.value?.count).to(equal(3))
             }
